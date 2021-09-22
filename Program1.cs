2eff19ff5ffb2076dbdbc9e914a7a93e48a1d086
naveen962 <mrn28897@gmail.com>
2021-09-21 19:56:05 -0700

    calculating the employee wages program is added

---
+++ Program1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+				//declaring variables
+            int Fulltime = 1;
+            int Emp_Wage_Per_Hour = 20;
+            int Emp_Hours = 0;
+				//object created
+            Random ran = new Random();
+            int Empcheck=ran.Next(0, 2);
+				//condition
+            if (Empcheck==Fulltime)
+            {
+                Emp_Hours = 8;
+            }
+            else
+            {
+                Emp_Hours = 0;
+            }
+
+            int EmployeeWage = Emp_Hours* Emp_Wage_Per_Hour;
+            Console.WriteLine("EMPLOYEE WAGES =" + EmployeeWage);
+        }
+    }
+}
