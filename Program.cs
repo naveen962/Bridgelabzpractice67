3bf08225c4330588e4e13cc443335bad047ec92c
naveen962 <mrn28897@gmail.com>
2021-09-23 02:41:58 -0700

    Add part time employee program is added sucessfully

--- Program.cs
+++ Program.cs
@@ -1,34 +1,14 @@
 ﻿using System;
 
-namespace EmployeeWage
+namespace EmpWages
 {
     class Program
     {
         static void Main(string[] args)
         {
-            const int Fulltime = 1;
-            const int Parttime = 0;
-            int Emp_Wage_Per_Hour = 20;
-            int Emp_Hours = 0;
-            Random ran = new Random();
-            int Empcheck=ran.Next(0, 2);
-          switch (Empcheck)
-            {
-                case Fulltime:
-                    Emp_Hours = 8;
-                    break;
+           
+            UseCase4.AddPartimeEmp();
 
-                case Parttime:
-                    Emp_Hours = 4;
-                    break;
-                default:
-                    Console.WriteLine("ABSENT");
-                    break;
-
-            }
-
-            int EmployeeWage = Emp_Hours* Emp_Wage_Per_Hour;
-            Console.WriteLine("EMPLOYEE WAGES =" + EmployeeWage);
         }
     }
 }
